cc4c27894215c3dd1dfeaefb524e3c652b05f031
Mohd Faizan <42490239+Mohdfaizan-3@users.noreply.github.com>
2023-02-27 14:22:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const textElement = document.querySelector("#evaluatedText");
 const h3Element = document.querySelector("#wordCount");
 
-textElement.addEventListener('input',()=>{
-	let sentence = e.target.value;
-	let count = sentence.split(" ").trim().length;
+textElement.addEventListener('input',(event)=>{
+	let sentence = event.target.value;
+	let count = sentence.trim().split(/\s+/).length;
 	h3Element.textContent = count;
-	alert('dsf')
+	
 })
